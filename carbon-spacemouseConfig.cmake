6cf73b05d992989457bd694419bf1b5024757d6d
Auðunn Andri Ólafsson <audunn@ccpgames.com>
2026-05-07 09:41:26 +0000

    add find_dependency calls for transitive deps

--- carbon-spacemouseConfig.cmake
+++ carbon-spacemouseConfig.cmake
@@ -1,1 +1,6 @@
+include(CMakeFindDependencyMacro)
 include("${CMAKE_CURRENT_LIST_DIR}/carbon-spacemouse.cmake")
+
+
+find_dependency(Python3 COMPONENTS Development REQUIRED)
+find_dependency(3dxwaresdk REQUIRED)
