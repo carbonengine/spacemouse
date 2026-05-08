0e2e21227210b4053d4c93428b3faf8361a22639
Auðunn Andri Ólafsson <audunn@ccpgames.com>
2026-05-08 09:17:00 +0000

    replace placeholder text with spacemouse

--- .teamcity/_Self.kt/buildTypes/PublishToPerforce.kt
+++ .teamcity/_Self.kt/buildTypes/PublishToPerforce.kt
@@ -27,7 +27,7 @@ class Publish(perforce_publish_path: String) : BuildType({
         param("env.TC_BUILD_URL", "%teamcity.serverUrl%/viewLog.html?buildId=%teamcity.build.id%")
         text("eve_branch_shortname", "", label = "Branch Name", description = """The name of the branch, for example MAINLINE""", display = ParameterDisplay.PROMPT, allowEmpty = false)
         param("env.TC_BUILDID", "%teamcity.build.id%")
-        param("env.TC_BUILD_NUMBER", "Carbon Template #%build.number%")
+        param("env.TC_BUILD_NUMBER", "Carbon Spacemouse #%build.number%")
         param("env.P4PORT", "p4is.ccp.ad.local:1666")
         param("env.TC_EVE_BRANCH_SHORTNAME", "%eve_branch_shortname%")
         param("env.TC_EVE_PROJECT", "%project%")
